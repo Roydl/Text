716733165030b9c103cacf2d7d4839d6b38c972a
Si13n7 <admin@si13n7.com>
2021-05-13 16:48:36 +0200

    Add code analysis suppression file.

---
+++ src/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "ReSharper: EmptyConstructor", Scope = "member", Target = "~M:Roydl.Text.BinaryToText.Base02.#ctor")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "ReSharper: EmptyConstructor", Scope = "member", Target = "~M:Roydl.Text.BinaryToText.Base08.#ctor")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "ReSharper: EmptyConstructor", Scope = "member", Target = "~M:Roydl.Text.BinaryToText.Base10.#ctor")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "ReSharper: EmptyConstructor", Scope = "member", Target = "~M:Roydl.Text.BinaryToText.Base16.#ctor")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "ReSharper: EmptyConstructor", Scope = "member", Target = "~M:Roydl.Text.BinaryToText.Base32.#ctor")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "ReSharper: EmptyConstructor", Scope = "member", Target = "~M:Roydl.Text.BinaryToText.Base64.#ctor")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "ReSharper: EmptyConstructor", Scope = "member", Target = "~M:Roydl.Text.BinaryToText.Base85.#ctor")]
+[assembly: SuppressMessage("CodeQuality", "IDE0079:Remove unnecessary suppression", Justification = "ReSharper: EmptyConstructor", Scope = "member", Target = "~M:Roydl.Text.BinaryToText.Base91.#ctor")]
