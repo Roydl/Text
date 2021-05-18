9a24ba28c21cb12cfc1b1f7b362303f8866d9a1f
Si13n7 <admin@si13n7.com>
2021-05-18 16:43:11 +0200

    Update test cases.

---
+++ test/OtherTests.cs
@@ -0,0 +1,64 @@
+﻿namespace Roydl.Text.Test
+{
+    using System;
+    using System.Linq;
+    using NUnit.Framework;
+
+    public class OtherTests
+    {
+        private static readonly TestCaseData[] Rot13TestData =
+        {
+            new(TestVarsType.TestString, "Grfg"),
+            new(TestVarsType.QuoteString, "Jr xabj jung jr ner, ohg xabj abg jung jr znl or."),
+            new(TestVarsType.RangeString, null),
+        };
+
+        [Test]
+        [TestCaseSource(nameof(Rot13TestData))]
+        public void Rot13(TestVarsType varsType, string expectedRotated)
+        {
+            string original;
+            switch (varsType)
+            {
+                case TestVarsType.TestString:
+                    original = TestVars.TestStr;
+                    break;
+                case TestVarsType.QuoteString:
+                    original = TestVars.QuoteStr;
+                    break;
+                case TestVarsType.RangeString:
+                    original = TestVars.RangeStr;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(varsType), varsType, null);
+            }
+            var rotated = TextConvert.Rot13(original);
+            if (varsType != TestVarsType.RangeString)
+                Assert.AreEqual(expectedRotated, rotated);
+            var normalized = TextConvert.Rot13(rotated);
+            Assert.AreEqual(original, normalized);
+        }
+
+        [Test]
+        [Description("***WIP: Test not strong enough yet!")]
+        public void FormatSeparators()
+        {
+            var original = $"{TextSeparator.HorizontalTab}{TextSeparator.HorizontalTab}{TextSeparator.HorizontalTab}{TestVars.QuoteStr}";
+            foreach (var str in TextSeparator.AllNewLineStrings)
+            {
+                original += str;
+                original += TextSeparator.WindowsDefault;
+            }
+            for (var i = 0; i < 3; i++)
+                original += original;
+
+            var allCount = original.Count(TextVerify.IsLineSeparator);
+            var formatted = TextConvert.FormatSeparators(original, TextNewLine.LineFeed);
+            Assert.AreEqual(allCount - 72, formatted.Count(TextVerify.IsLineSeparator));
+
+            var lineFeedCount = formatted.Count(c => c == TextSeparator.LineFeedChar);
+            var normalized = TextConvert.FormatSeparators(formatted);
+            Assert.AreEqual(formatted.Length + lineFeedCount, normalized.Length);
+        }
+    }
+}
